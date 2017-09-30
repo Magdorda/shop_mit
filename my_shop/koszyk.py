38c7de15c525747eacf14aff010778ee18c1d4ce
marstom <marstomek@gmail.com>
2017-09-30 14:27:13 +0200

    drukowanie koszyka

--- my_shop/koszyk.py
+++ my_shop/koszyk.py
@@ -6,7 +6,7 @@ class Cart:
         id, name, price, description = prod
         product = {'id': id, 'name': name, 'price': price, 'description': description, 'quantity': quantity}
         self.content.append(product)
-        print(self.content)
+        # print(self.content)
 
     def delete_product(self):
         pass
@@ -14,5 +14,20 @@ class Cart:
     def update_product(self):
         pass
 
-    def display(self):
-        pass
+    def get_cart_content(self):
+        text ='\nProdukty w koszyku: \n'
+        for el in self.content:
+            text += '{} \t ilość: {}\n'.format(el['name'], el['quantity'])
+        return text
+
+    def __str__(self):
+        #drukuj ładnie produkty
+        return self.get_cart_content()
+
+
+
+if __name__ == '__main__':
+    cr = Cart()
+    cr.add_product([1,'krówki',22,'to sa krowki'], 12)
+    cr.add_product([1, 'serniczek', 224, 'to sa ser'], 12)
+    print(str(cr))
--- my_shop/menu.py
+++ my_shop/menu.py
@@ -1,6 +1,8 @@
 from my_shop.baza import DataSQL
 from my_shop.koszyk import Cart
 
+import os
+
 class Menu:
     def __init__(self, db_file):
         self.db_file = db_file
@@ -11,17 +13,25 @@ class Menu:
         3. Podgląd koszyka
         4. Zakończ zamówienie
         5. Pomoc"""
-        self.switch = {'1': self.products_list, '2': self.add_to_cart}
+        self.switch = {'-1': self.error_message,
+                        '1': self.products_list,
+                        '2': self.add_to_cart,
+                        '3': self.show_cart
+                       }
         self.cart = Cart()
 
-    def hello(self):
-        pass
+    def error_message(self):
+        print('Niedozwolona operacja! Spróbuj jeszcze raz.')
 
     def products_list(self):
         print('Lista produktów: ')
         with DataSQL(self.db_file) as db:
             products = db.product_list_string()
             print(products)
+
+    def show_cart(self):
+        content = self.cart.get_cart_content()
+        print(content)
 
     def add_to_cart(self):
         print('Dodawanie produktu do koszyka.')
@@ -34,17 +44,23 @@ class Menu:
         quantity = int(quantity)
         self.cart.add_product(prod, quantity)
 
-
-
+    def user_input(self):
+        choice_id = str(input('Wybierz czynnosc: ')).strip()
+        if choice_id not in self.switch.keys():
+            return '-1'
+        print('Wybrano: ', choice_id)
+        return choice_id
 
     def run(self):
         print(self.hello)
         print(self.options)
 
         while True:
+            # os.system('clear')
             # może dodamy czyszczenie ekranu
-            choice_id = input('Wybierz czynnosc: ')
-            print('Wybrano: ', choice_id)
+            #choice_id = str(input('Wybierz czynnosc: ')).strip()
+            choice_id = self.user_input()
+
             choice = self.switch[choice_id]
             choice()
 
