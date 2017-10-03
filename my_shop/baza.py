44141c866688631f3f1611bcf0e31f056110a082
marstom <marstomek@gmail.com>
2017-10-03 09:32:37 +0200

    *All database get methods now returns dictionaries *Create sql querries

--- my_shop/baza.py
+++ my_shop/baza.py
@@ -1,6 +1,10 @@
 import sqlite3
 
+#TODO all methods should return dictionaries
 class DataSQL:
+    '''
+    all get methods return dictionaries with columnnames
+    '''
     def __init__(self, file_name):
         self.file_name = file_name
         self.connection = None
@@ -9,13 +13,25 @@ class DataSQL:
     def __enter__(self):
         self.connection = sqlite3.connect(self.file_name)
         self.cursor = self.connection.cursor()
+        self.cursor.row_factory = self.dict_factory
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
 
+    @staticmethod
+    def dict_factory(cursor, row):
+        '''
+        method that create dictionary from sql
+        '''
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
     def get_product_list(self):
         result = []
+        names = ('id','name','price','description')
         for row in self.cursor.execute('SELECT * FROM product'):
             result.append(row)
         return result
@@ -33,11 +49,13 @@ class DataSQL:
 
     def get_product(self, id):
         self.cursor.execute('SELECT * FROM product WHERE id = ?', id)
+        names = ('id', 'name', 'price', 'description')
         result = self.cursor.fetchone()
         return result
 
     def get_customer(self, name):
         self.cursor.execute("SELECT * FROM customer WHERE user_name = ?", (name,))
+        fields = ['id', 'name','password']
         result = self.cursor.fetchone()
         return result
 
@@ -47,16 +65,15 @@ class DataSQL:
 
     def add_cart_to_order(self,username, password, cart):
         #TODO get customer, insert to db user id, order id for each item
+        customer = self.get_customer(username)
+        id = customer['id']
         for item in cart:
             print(item)
 
 ####temporary tests
 
-def _display_user():
+def _tst_get_customer():
     with DataSQL('shop_data_base.db') as db:
-        prod = db.get_product('2')
-        print(prod)
-
         cust = db.get_customer('tomek')
         print(cust)
 
@@ -65,7 +82,12 @@ def _sample_cart_content():
     with DataSQL('shop_data_base.db') as db:
         db.add_cart_to_order('tomek', 'tomek',content)
 
+def _tst_product_lits():
+    with DataSQL('shop_data_base.db') as db:
+        print(db.get_product_list())
     dsq = DataSQL('')
+
 if __name__ == '__main__':
-    # _display_user()
-    _sample_cart_content()
+    _tst_get_customer()
+    # _sample_cart_content()
+    # _tst_product_lits()
